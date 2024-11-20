b2a2a38cd626ad3fb8d779ea8592ab5d1b7f9dd9
imrulkayes2 <ikayes.eee@gmail.com>
2024-11-20 20:41:17 +0600

    motors bay app create

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+
+function App() {
+  
+  return (
+    <>
+      
+      <h1 className='h-1 text-6xl text-left text-red-500'>The Journey to <br /> a Perfection</h1>
+      
+      <h4 className='align-text-top'>Top performance requires more than perfact condittions especialy just luck</h4>
+    </>
+  )
+}
+
+export default App
